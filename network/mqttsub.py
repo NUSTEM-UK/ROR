809ead512465f5e66b60d57c9aa31bc339557671
Jonathan Sanderson <lists@quernstone.com>
2017-12-08 14:21:14 +0000

    Fixes for mqttsub.py, python3

--- network/mqttsub.py
+++ network/mqttsub.py
@@ -4,18 +4,21 @@ import paho.mqtt.client as mqtt
 
 def on_connect(client, userdata, rc):
     """Connect to MQTT broker."""
-    print "Connected with result code: " + str(rc)
+    print("Connected with result code: " + str(rc))
     client.subscribe("#")
 
 
 def on_message(client, userdata, msg):
     """Output diagnostic when message sent via broker."""
-    print "Topic:", msg.topic + '  :  Message: ' + msg.payload
+    print("Topic:", msg.topic + '  :  Message: ' + msg.payload)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# client.connect('10.0.1.5', 1883)
-client.connect('127.0.0.1', 1883)
+# For reasons that are completely unclear to me, connecting to 127.0.0.1
+# throws a TypeError in on_connect: needs 3 arguments, four given.
+# Adding `self` to the definition doesn't help. I don't understand.
+client.connect('10.0.1.5', 1883)
+# client.connect('127.0.0.1', 1883)
 client.loop_forever()
