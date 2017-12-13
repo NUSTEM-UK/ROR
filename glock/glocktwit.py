77ae387381bd94cf963972dff75b6d4217bdedcb
llewmihs <joeshimwell@gmail.com>
2017-12-13 15:38:45 +0000

    It now tweets back!

--- glock/glocktwit.py
+++ glock/glocktwit.py
@@ -1,5 +1,5 @@
 from twitcreds import *
-from twython import TwythonStreamer
+from twython import TwythonStreamer, Twython
 from songsearcher import *
 import paho.mqtt.client as mqtt
 from rtttl import RTTTL
@@ -10,29 +10,39 @@ mqtt_server = "10.0.1.5"
 # mqtt_server = "127.0.0.1"
 
 
-def message(payload):
+def message(topic, payload):
     """Abstract out MQTT connection.
     Since it has to be done for each message, wrap it in a function.
     """
     mqttc.connect(mqtt_server, 1883)
-    mqttc.publish("orchestra/cue", payload)
+    mqttc.publish(topic, payload) #"orchestra/cue"
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
-            print(data['text'])
+            #print(data)
+            #print(data['text'])
+            userData = data['user']
+            print(type(userData))
+            print(userData['screen_name'])
+            
             a,b,c = searcher(data['text'])
             print(a, b)
             print("Checking song duration:")
             tune = RTTTL(c)
             totalTime = 0
             for freq, msec in tune.notes():
-                print(freq, msec)
                 totalTime += msec
             print(totalTime)
             print("Sending RTTTL file to MQTT")
-            message(c)
-            print("Complete.")
+            message("orchestra/song", a)
+            time.sleep(0.2)
+            message("orchestra/handle", "@"+userData['screen_name'])
+            time.sleep(0.2)
+            message("orchestra/cue", c)
+            tweet = "@" + userData['screen_name'] + " Thanks for your song request! We're now playing: " + a +  ". Merry Christmas from @nustem_uk"
+            print(tweet)
+            twitter.update_status(status=tweet)
 
 
     def on_error(self, status_code, data):
@@ -44,7 +54,7 @@ class MyStreamer(TwythonStreamer):
 
 if __name__ == "__main__":
     stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
+    twitter = Twython(APP_KEY, APP_SECRET,OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
     while True:
         print("Listening to Twitter")
         stream.statuses.filter(track='#copperglock')
