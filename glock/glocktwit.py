f0267ffbbf29d856ba86cf25b8a21d94cb1b9b38
llewmihs <joeshimwell@gmail.com>
2017-12-13 10:40:14 +0000

    Intergrating MQTT subs, but it ain't working

--- glock/glocktwit.py
+++ glock/glocktwit.py
@@ -7,6 +7,7 @@ mqttc = mqtt.Client()
 mqtt_server = "10.0.1.5"
 # mqtt_server = "127.0.0.1"
 
+
 def message(payload):
     """Abstract out MQTT connection.
     Since it has to be done for each message, wrap it in a function.
@@ -33,5 +34,8 @@ class MyStreamer(TwythonStreamer):
 
 if __name__ == "__main__":
     stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
     while True:
+        print("Listening to Twitter")
         stream.statuses.filter(track='#copperglock')
+        
