8433dc8a48fa213bd356999457dc92da1faf7cac
Jonathan Sanderson <lists@quernstone.com>
2017-12-13 11:23:50 +0000

    Rigntone MQTT controller - initial commit (working!)

--- controller-ringtone/controller-ringtone.py
+++ controller-ringtone/controller-ringtone.py
@@ -1,6 +1,6 @@
-"""Software piano test.
+"""Robot Orchestra controller - taking RTTTL ringtone requests from Twitter.
 
-Pulled mostly from Pimoroni Piano HAT code:
+Elements drawn from Pimoroni Piano HAT code:
 https://github.com/pimoroni/Piano-HAT/blob/master/examples/simple-piano.py
 """
 
@@ -12,6 +12,9 @@ from time import sleep
 from sys import exit
 from rtttl import RTTTL
 from rttllist import songdict
+from threading import Timer
+import paho.mqtt.client as mqtt # requires `pip install paho-mqtt`
+import numpy as np
 # from math import log2, pow # Python3 has a log2
 from math import log, pow
 
@@ -20,7 +23,7 @@ try:
 except ImportError:
     exit("This script requires the pygame module\nInstall with: sudo pip install pygame")
 
-BANK = os.path.join(os.path.dirname(__file__), "../piano_pi/sounds")
+BANK = os.path.join(os.path.dirname(__file__), "sounds")
 
 NOTE_OFFSET = 0
 FILETYPES = ['*.wav', '*.ogg']
@@ -33,6 +36,11 @@ octaves = 0
 A4 = 440
 C0 = A4*pow(2, -4.75)
 name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+mqttc = mqtt.Client()
+# mqtt_server = "127.0.0.1"
+mqtt_server = "10.0.1.5"
+
 
 pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.mixer.init()
@@ -112,56 +120,64 @@ def freq_to_note(freq):
     n = h % 12
     return name[int(n)], int(octave)
 
+def on_connect(self, client, userdata, rc):
+    """Connect to MQTT broker & subscribe to cue channel."""
+    print("Connected with result code: " + str(rc))
+    self.subscribe("orchestra/cue")
+
+def message(topic, payload):
+    """Abstract out MQTT connection.
+
+    Since it has to be done for each message, wrap it in a function.
+    """
+    mqttc.connect(mqtt_server, 1883)
+    mqttc.publish("orchestra/" + topic, payload)
+
+def playset(beatset):
+    """Sends the current beat to all 8 channels at once."""
+    message("playset", beatset)
+
+def on_message(client, userdata, msg):
+    """Handle incoming playback cue."""
+    notedict = {"C":36, "C#":37, "D":38, "D#":39, "E":40, "F":41, "F#":42, "G":43, "G#":44, "A":45, "A#":46, "B":47}
+    channeldict = {"C":0, "C#":0, "D":1, "D#":1, "E":2, "F":3, "F#":3, "G":4, "G#":4, "A":5, "A#":5, "B":6}
+
+    # print("Topic:", msg.topic + '  :  Message: ' + msg.payload)
+    print(msg.topic, msg.payload)
+
+    tune = RTTTL(msg.payload)
+
+    for freq, msec in tune.notes():        
+        # print(freq, msec)
+        if freq != 0.0:
+            note, oct = freq_to_note(freq)
+            print(note, oct)
+            play_beats = list("00000000") # fresh playlist. List so mutable
+            play_beats[channeldict[note]] = "1"
+            playset(''.join(play_beats)) # Command the orchestra!
+            handle_note(notedict[note], oct)
+            sleep(msec/1000.0)
+        else:
+            print('Rest!')
+            sleep(msec/1000.0)
+
+    # Make sure the last note plays
+    sleep(0.3)
+    print(">>> Playback complete!")
+
 load_samples(patches[patch_index])
 
-# Old test code I haven't deleted yet.
-# This really should have been in /experiments, but there you go.
 
-# scale_up(8, 0.1)
-# scale_down(8, 0.1)
-# sleep(1)
-
-# handle_note(36, 1)
-# sleep(1)
-# handle_note(48, 0)
-# sleep(1)
-
-# scale = [36, 38, 40, 41, 43, 45, 47, 48]
-# for note in scale:
-#     handle_note(note-1, 0)
-#     sleep(0.3)
-# sleep(1)
-# scale2 = [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48]
-# for note in scale2:
-#     handle_note(note-1, 0)
-#     sleep(0.3)
-
-# sleep(1)
-# scale3 = {"C":36, "D":38, "E":40, "F":41, "G":43, "A":45, "B":47, "C2":48}
-# for note in "C,D,E,F,G,A,B,C2".split(","):
-#     print("Playing {}".format(note))
-#     handle_note(scale3[note], 0)
-#     sleep(0.3)
-
-notedict = {"C":36, "C#":37, "D":38, "D#":39, "E":40, "F":41, "F#":42, "G":43, "G#":44, "A":45, "A#":46, "B":47}
-channeldict = {"C":0, "C#":0, "D":1, "D#":1, "E":2, "F":3, "F#":3, "G":4, "G#":4, "A":5, "A#":5, "B":6}
-
-tune = RTTTL(songdict['Oh little star of bethlehem'])
+# tune = RTTTL(songdict['Oh little star of bethlehem'])
 # tune = RTTTL(songdict['Jingle Bells'])
 # tune = RTTTL(songdict['Frosty the snowman'])
 # tune = RTTTL(songdict["I'm dreaming of a white christmas"])
 # tune = RTTTL(songdict['Take On Me'])
 # tune = RTTTL(songdict['Airwolf Theme'])
 
-for freq, msec in tune.notes():
-    print(freq, msec)
-    if freq != 0.0:
-        note, oct = freq_to_note(freq)
-        print(note, oct)
-        handle_note(notedict[note], oct)
-        sleep(msec/1000.0)
-    else:
-        sleep(msec/1000.0)
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
 
-# Make sure the last note plays
-sleep(0.3)
+client.connect(mqtt_server, 1883)
+client.loop_forever()
