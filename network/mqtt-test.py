1d0ba2dca123f34594dd0e14a85ad0fcbc0d0f20
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2017-12-14 11:06:38 +0000

    More tweaks to song list.

--- network/mqtt-test.py
+++ network/mqtt-test.py
@@ -14,6 +14,6 @@ def message(topic, payload):
 # message("song", "Short name")
 # message("handle", "@jjsanderson")
 # sleep(1)
-message("song", "We Wish You a Merry Christmas")
+message("song", "Jingle Bells")
 message("handle", "Joe. Always Joe.")
-message("cue", "ThePriso:d=4,o=6,b=160:8c#7,8b,2c#.,8b,8a,2b.,8a,g#,8a#,8f#,8d,8c#,f#,d,8c#,8f#,8d,8c#,f#,8c#,8b,c#7,b,a,g#,c#,8e,8c#,d#,8a,8b,c#7,b,a,g#,c#,8e,8c#,8d#,8a,b,2c#7,b,8a,8g#,c#,8e,8c#,d#,8e,8f#,g#.,g#.,g#,8g#,8p,8g#,8f#,8g#,8p,8d#,8c#,8d#,8p,8g#,8f#,8g#,8p,8g#7,8f#7,g#7,8f#7,8e7,f#7,8e7,8d#7,8c#7,8g#,f#,c#,8g#7,8f#7,g#7,8f#7,8e7,f#7,8e7,8d#7,8c#7,8e,b5,2a5")
+message("cue", "AllIWant:d=4,o=6,b=160:c5,e5,g5,8b5,c,b.5,8a5,g.5,d,c,8c,b5,c,b5,8a5,2g5,a5,c,8d,e,d,c,a.5,f5,8g#5,c.,8d,d#,d,a#5,g#.5,c,d,b5,8c,a5,b5,2g#5,c,d,b5,8c,a5,b5,2g#5,g5,a5,8c,g,f,8g,2f,e,d,c,a5,g#5,2d,e,8d.,2c.")
